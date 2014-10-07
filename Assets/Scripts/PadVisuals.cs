732cb80d6022b94fb5badeb7e0f5488bccd8b572
Andre Duvoisin <duvoisin@usc.edu>
2014-10-06 23:13:06 -0700

    Particles work. Starting prefabs. ez

---
+++ Assets/Scripts/PadVisuals.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PadVisuals : MonoBehaviour
+{
+	public GameObject mBall;
+	public GameObject mParticleObject;
+	private ParticleSystem[] mParticleSystem;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		mParticleObject = Instantiate (mParticleObject, transform.position, transform.rotation) as GameObject;
+		mParticleSystem = mParticleObject.GetComponentsInChildren<ParticleSystem>();
+		foreach (ParticleSystem pSys in mParticleSystem)
+		{
+			pSys.Clear ();
+			pSys.Play ();
+		}
+	}
+}
