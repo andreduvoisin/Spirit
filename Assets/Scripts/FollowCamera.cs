3b427d53346fe2683179b4bf57f011c1b120c624
U-TianyuLang-PC\TianyuLang <tlang@usc.edu>
2014-10-07 00:40:19 -0700

    no exclamation mark this time

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -3,14 +3,31 @@ using System.Collections;
 
 public class FollowCamera : MonoBehaviour {
 	public GameObject gameObject;
+	public float mSpringConstant = 0.0f;
+	public float mDampConstant = 0.0f;
+	public float mForwardDistFromBall = 5.0f;
+	public float mUpDistFromBall = 5.0f;
+	private Vector3 mIdealCameraPos = new Vector3();
+	private Vector3 mVelocity = new Vector3();
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		mDampConstant *= Mathf.Sqrt(mSpringConstant);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		//transform.position = gameObject.transform.position + new Vector3(0, 1, -10);
+	void FixedUpdate () 
+	{
+		transform.rotation = Quaternion.LookRotation(gameObject.transform.position - transform.position);
+
+		Vector3 direction = gameObject.transform.rigidbody.velocity;
+		direction.Normalize();
+		mIdealCameraPos = gameObject.transform.position - direction * mForwardDistFromBall;
+		mIdealCameraPos += transform.up * mUpDistFromBall;
+		Vector3 displacement = transform.position - mIdealCameraPos;
+		Vector3 springAccel = (-mSpringConstant * displacement) - (mDampConstant * mVelocity);
+		mVelocity += springAccel * Time.deltaTime;
+		transform.position += mVelocity * Time.deltaTime;
 	}
 }
